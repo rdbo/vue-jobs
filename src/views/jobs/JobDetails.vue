47f2720c210f14c5138afed37b69c7fae04ca97f
Rdbo <57117082+rdbo@users.noreply.github.com>
2023-01-28 16:03:28 -0300

    Add files via upload

---
+++ src/views/jobs/JobDetails.vue
@@ -0,0 +1,20 @@
+<template>
+        <div class="jobs">
+            <h1>Job Details</h1>
+            <p>The job ID is: {{ id }}</p>
+        </div>
+</template>
+
+<script>
+
+export default {
+    name: 'JobDetails',
+    props: ["id"],
+    components: {  },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
---
+++ src/views/jobs/Jobs.vue
@@ -0,0 +1,47 @@
+<template>
+        <div class="jobs">
+            <h1>Jobs</h1>
+            <div v-for="job in jobs" :key="job">
+                <router-link :to="{ name: 'job_details', params: { id: job.id } }">
+                    <h2>{{ job.title }}</h2>
+                </router-link>
+            </div>
+        </div>
+</template>
+
+<script>
+
+export default {
+    name: 'JobsView',
+    components: {  },
+    data() {
+        return {
+            jobs: [
+                { id : 0, title: 'UX Developer', details: 'description' },
+                { id : 1, title: 'Web Developer', details: 'description' },
+                { id : 2, title: 'Vue Developer', details: 'description' }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+    .jobs h2 {
+        background: #f4f4f4;
+        padding: 20px;
+        border-radius: 10px;
+        margin: 10px auto;
+        max-width: 600px;
+        cursor: pointer;
+        color: #444;
+    }
+
+    .jobs h2:hover {
+        background: #ddd;
+    }
+
+    .jobs a {
+        text-decoration: none;
+    }
+</style>
